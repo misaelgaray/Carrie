a99b89b3919a5b42624c7bac6a3532d3dcdc6efb
MisaelGaray <misael.garay@hotmail.com>
2016-08-07 03:33:12 -0500

    Latest Release with Client side built

--- CarrierIntegrator/Controllers/CarrieController.cs
+++ CarrierIntegrator/Controllers/CarrieController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarrierIntegrator.Services;
 
 namespace CarrierIntegrator.Controllers
 {
     public class CarrieController : Controller
     {
+        CarreerDataBaseEntities1 db = new CarreerDataBaseEntities1();
+        SessionService ss = new SessionService();
         // GET: Carrie
         public ActionResult Index()
         {
@@ -15,6 +18,73 @@ namespace CarrierIntegrator.Controllers
         }
 
         public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Areas()
+        {
+            var carreras = db.areas_car.Select(e => new
+            {
+                id_area = e.id_area,
+                nombre_area = e.nombre_area
+            });
+
+            return Json(carreras, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Carreras(int id)
+        {
+            var carreras = db.Carreras.Where(i => i.fk_area == id).Select(e => new
+            {
+                id_carrera = e.id_carrera,
+                nombre_carrera = e.nombre_carrera
+            });
+
+            return Json(carreras, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(string email, string pass)
+        {
+            if(email == null || pass == null)
+            {
+                TempData["login"] = "void";
+                return RedirectToAction("LogIn");
+            }
+
+            int validado = ss.LogIn(email, pass);
+            Session["usersession"] = validado;
+
+            if (validado != 0)
+            {
+                TempData["login"] = "success";
+                return RedirectToAction("Main");
+            }
+
+            TempData["login"] = "denied";
+            return RedirectToAction("LogIn");
+        }
+
+        public ActionResult Main()
+        {
+            int usersession = 0;
+            if (Session["usersession"] != null)
+            {
+                usersession = Convert.ToInt32(Session["usersession"]);
+            }
+
+            return View();
+        }
+
+        public ActionResult Back(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Foward()
         {
             return View();
         }
---
+++ CarrierIntegrator/Services/SessionService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarrierIntegrator.Models;
+
+namespace CarrierIntegrator.Services
+{
+    public class SessionService
+    {
+        CarreerDataBaseEntities1 db;
+
+        public SessionService()
+        {
+            db = new CarreerDataBaseEntities1();
+        }
+
+        public int LogIn(string user, string pass)
+        {
+            int session = 0;
+            try
+            {
+                var sess = (from val in db.Usuarios where 
+                           val.correo_usuario == user && val.pass_usuario == pass
+                           select val).Single();
+                if(sess != null)
+                {
+                    //By default session is Misaels Session
+                    Tsession new_session = StaticObjects.session(sess.id_usuario, DateTime.Now);
+                    db.Tsession.Add(new_session);
+
+                    try
+                    {
+                        db.SaveChanges();
+                        var result = (from ses in db.Tsession select ses.id_session).Max();
+                        session = result;
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+
+                }
+            }
+            catch
+            {
+                return session;
+            }
+
+            return session;
+        }
+    }
+}
